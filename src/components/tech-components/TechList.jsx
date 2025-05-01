b386c252b98137a18a275298c501b99ee20de327
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-01 22:58:54 +0200

    added media queries to tech section

--- src/components/tech-components/TechList.jsx
+++ src/components/tech-components/TechList.jsx
@@ -3,6 +3,12 @@ import styled from "styled-components"
 const TechListStyled = styled.p`
   margin: 1rem 0 4rem 0;
   padding: 0 1rem 0 1rem;
+
+  @media (min-width: 1024px) {
+    margin: 2rem 0 2rem 0;
+    font-size: 1.125rem;
+    padding: 0 20rem 0 20rem;
+  }
 `
 
 const TechList = () => {
--- src/typography/SectionTitle.jsx
+++ src/typography/SectionTitle.jsx
@@ -4,9 +4,13 @@ const SectionTitleStyled = styled.h2`
   font-size: 3rem;
   font-weight: 700;
   line-height: 3.5rem;
+
+  @media (min-width: 1024px) {
+    font-size: 5rem;
+  }
 `
 
-const SectionTitle = ({ title, color }) => {
+const SectionTitle = ({ title }) => {
   return (
     <SectionTitleStyled>{title}</SectionTitleStyled>
   )
