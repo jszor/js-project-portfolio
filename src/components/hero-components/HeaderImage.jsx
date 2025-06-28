6f29de8a48639d6e45926cc19a07897dd22e97bf
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-06-29 00:01:08 +0300

    add media query for header image

--- src/components/hero-components/HeaderImage.jsx
+++ src/components/hero-components/HeaderImage.jsx
@@ -3,11 +3,15 @@ import headerImage from "../../assets/header_img.webp"
 
 const HeaderImageStyled = styled.img`
   display: block;
-  max-width: 450px;
+  max-width: 250px;
   align-self: center;
   margin-top: 3.5rem;
   border-radius: 15px;
   border: 2px solid black;
+
+  @media (min-width: 768px) {
+    max-width: 450px;
+  }
 `
 
 const HeaderImage = () => {
